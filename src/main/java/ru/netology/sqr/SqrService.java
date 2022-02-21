a879f50f38f364c01ae973fd4fb63668d0d312a8
unknown <shuklinavaleria@yandex.ru>
2022-02-21 17:07:34 +0300

    modificatoin v.3

--- src/main/java/ru/netology/sqr/SqrService.java
+++ src/main/java/ru/netology/sqr/SqrService.java
@@ -1,13 +1,15 @@
 package ru.netology.sqr;
 
 public class SqrService {
-    public int sqrtCeil(int min,int max){
+    public int sqrtCeil(int min, int max) {
         int count;
         count = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= min && i * i <= max) {
-                count++;}
-        }return count;
+                count++;
+            }
+        }
+        return count;
 
     }
 }
--- src/test/java/ru/netology/sqr/SqrServiceTest.java
+++ src/test/java/ru/netology/sqr/SqrServiceTest.java
@@ -1,8 +1,6 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,7 +14,7 @@ class SqrServiceTest {
         int expected = 3;
         int actual = service.sqrtCeil(200, 300);
 
-        assertEquals (expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -27,7 +25,7 @@ class SqrServiceTest {
         int expected = 13;
         int actual = service.sqrtCeil(100, 500);
 
-        assertEquals (expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -38,18 +36,7 @@ class SqrServiceTest {
         int expected = 5;
         int actual = service.sqrtCeil(100, 200);
 
-        assertEquals (expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvSource (value ={"100/200, 100, 200, 5", "200/300, 200, 300, 3", "100/500, 100, 500, 13"})
-
-    void shouldSqrServiceParam(String testName, int min, int max, int expected) {
-        SqrService service = new SqrService();
-
-        int actual = service.sqrtCeil(min, max);
-
-        assertEquals (expected, actual);
+        assertEquals(expected, actual);
     }
 }
 
