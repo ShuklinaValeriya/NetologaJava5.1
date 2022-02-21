02bb1e129cfdf8dd2a770abb14ef20fe53d9283f
unknown <shuklinavaleria@yandex.ru>
2022-02-21 14:05:03 +0300

    modification v.2

--- src/main/java/ru/netology/sqr/SqrService.java
+++ src/main/java/ru/netology/sqr/SqrService.java
@@ -1,18 +1,14 @@
 package ru.netology.sqr;
 
 public class SqrService {
-
-
     public int sqrtCeil(int min,int max){
-        int count = 0;
+        int count;
+        count = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= min && i * i <= max) {
-                count++;
+                count++;}
+        }return count;
 
-                }
-
-            }
-
-        return count;
     }
 }
+
--- src/test/java/ru/netology/sqr/SqrServiceTest.java
+++ src/test/java/ru/netology/sqr/SqrServiceTest.java
@@ -1,20 +1,53 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SqrServiceTest {
 
     @Test
-    void shouldSqrService() {
+    void shouldSqrService200300() {
         SqrService service = new SqrService();
 
-        int min = 200;
-        int max = 300;
+        int count = 0;
         int expected = 3;
+        int actual = service.sqrtCeil(200, 300);
 
-        int actual = service.sqrtCeil(count);
+        assertEquals (expected, actual);
+    }
+
+    @Test
+    void shouldSqrService100500() {
+        SqrService service = new SqrService();
+
+        int count = 0;
+        int expected = 13;
+        int actual = service.sqrtCeil(100, 500);
+
+        assertEquals (expected, actual);
+    }
+
+    @Test
+    void shouldSqrService100200() {
+        SqrService service = new SqrService();
+
+        int count = 0;
+        int expected = 5;
+        int actual = service.sqrtCeil(100, 200);
+
+        assertEquals (expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource (value ={"100/200, 100, 200, 5", "200/300, 200, 300, 3", "100/500, 100, 500, 13"})
+
+    void shouldSqrServiceParam(String testName, int min, int max, int expected) {
+        SqrService service = new SqrService();
+
+        int actual = service.sqrtCeil(min, max);
 
         assertEquals (expected, actual);
     }
